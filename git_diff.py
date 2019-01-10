f2e1f85251ec8608fae19f1262703ab70273b78c
zbf <hgkdzbf6@163.com>
2019-01-10 23:19:30 +0800

    hello

--- git_diff.py
+++ git_diff.py
@@ -3,72 +3,128 @@ import math
 
 class DiffBlock:
     def __init__(self):
+        self.lost_index = []
+        self.more_index = []
+        self.diag_index = []
         pass
             
-    def calc(self,str, pattern):
-        # 5
-
-        # a = 'abcabc'
-        # b = 'bcbd'
-        # 4
-
-        # a = 'abcabc'
-        # b = 'abc'
-        # 3
-
-        N = len(str)
-        M = len(pattern)
-
-        MAX = max(0, M+N)
-        V = np.zeros((2*MAX+1), np.int)
-
-        dist = -1
-
-        for D in range(MAX):
-            for k in range(-D, D+1, 2):
-                if k == -D or k != D and V[k-1] < V[k+1]:
-                    x = V[k+1]
+    def calc(self,a, b):
+        Xs = []
+        X = {}
+        X[1] = 0
+        go_ahead = True
+        d = 0
+        the_max = len(a)+len(b)
+        while d <= the_max and go_ahead:
+            k=-d
+            while k<=d:
+                down = (k==-d or (k!=d and X[k-1]<X[k+1]))
+                if down:
+                    k_prev = k+1
                 else:
-                    x = V[k-1] + 1
+                    k_prev = k-1
+                # 起始位置
+                x_start = X[k_prev]
+                y_start = x_start - k_prev
+                # mid位置
+                if down:
+                    x_temp = x_start
+                else:
+                    x_temp = x_start + 1
+                y_temp = x_temp-k
 
-                y = x - k
+                x_end = x_temp
+                y_end = y_temp
+                
+                while x_end < len(a) and y_end < len(b) and a[x_end] == b[y_end] :
+                    x_end=x_end+1
+                    y_end=y_end+1
+                X[k] = x_end
 
-                while x < N and y < M and (y < 0 or str[x] == pattern[y]):
-                    x += 1
-                    y += 1
-
-                V[k] = x
-
-                if x >= N and y >= M:
-                    dist = D
+                # 到终点，找到了最终路线
+                if x_end>=len(a) and y_end >=len(b):
+                    go_ahead = False
                     break
+                k=k+2
+            # 如果有8个以上错误就抛弃，返回结果是0
+            Xs.append(X)
+            d=d+1
+        print(Xs)
+        # 回溯
+        path =[]
+        pt = [len(a),len(b)]
+        path.append(pt)
+        d = len(Xs)-1
+        while (pt[0]>0 or pt[1]>0) :
+            X = Xs[d]
+            k = pt[0] - pt[1]
+            x_end = X[k]
+            y_end = pt[0] - k
+            while x_end>0 and y_end>0 and x_end <= len(a) and y_end <= len(b) and a[x_end-1]==b[y_end-1]:
+                x_end = x_end - 1
+                y_end = y_end - 1
+                path.append([x_end, y_end])
+            down = (k==-d or (k!=d and X[k-1]<X[k+1]))
+            if down:
+                k_prev = k+1
+            else:
+                k_prev = k-1
+            print("hello")
+            print(path)
+            if k_prev == 2:
+                pass
+                print(path)
+                print(X)
+            x_start = X[k_prev]
+            y_start = x_start - k_prev
+            # path.append([x_start, y_start])
+            pt[0] = x_start
+            pt[1] = y_start
+            d=d-1
 
-            if dist != -1:
-                break
-        self.dist = dist
-        self.V = V
-        self.str_len = len(str)
-        self.max_len = MAX
-        self.pattern_len = len(pattern)
+        path = path[1:len(path)]
+        if len(path)>=2:
+            size = len(path)
+            i = size-2
+            while i>=0:
+                prev_point = path[i+1]
+                point = path[i]
+                res = self.calc2point(prev_point,point)
+                if res == "right":
+                    print("-{}".format(a[prev_point[0]]))
+                elif res == "down":
+                    print("+{}".format(b[prev_point[1]]))
+                elif res == "diag":
+                    print("={}".format(a[prev_point[0]]))
+                i=i-1
+        print(self.lost_index)
+        print(self.more_index)
+        print(self.diag_index)
+        print(path)
+        
 
-    def score(self):
-        print(self.dist)
-        print(self.V)
-        print(len(self.V))
-        print(self.str_len)
-        print(self.pattern_len)
-        the_sum = np.sum(np.array(self.V))
-        self.score = 1- math.exp(-self.str_len / (the_sum - self.str_len) - (the_sum - self.str_len)/self.str_len)
-        self.score = self.score - math.fabs(self.str_len - self.pattern_len)/ self.str_len
-        print(self.score)
+    def calc2point(self, p1, p2):
+        if p1[0] == p2[0] and p1[1]+1 == p2[1]:
+            # 如果少了字符的话，向下移动
+            self.lost_index.append(p1[0])
+            return "down"
+        elif p1[1] == p2[1] and p1[0]+1 == p2[0]:
+            # 如果多了字符，向右移动
+            self.more_index.append(p2[1])
+            return "right"
+        elif p1[0]+1 == p2[0] and p1[1]+1 == p2[1]:
+            self.diag_index.append(p1[0])
+            return "diag"
 
 def main():
-    
-    a = 'abcabca'
-    b = 'abcabca'
+    # a = 'GS150415232'
+    # b = 'GS150415234'
+    a = 'ABCDEFG'
+    b = 'BBCDEFG'
+    # b = 'hello world'
     diff_block = DiffBlock()
     diff_block.calc(a,b)
-    diff_block.score()
+    # diff_block.score()
 
 if __name__ == '__main__':
     main()
